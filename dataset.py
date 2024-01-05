48982453e85abb09116a2af7e4bffcd0ceaf1412
John Higgins <johnmatthiggins@gmail.com>
2024-01-04 22:19:08 -0800

    did trial run with decision tree classifier.
    accuracy = 52% :(

--- dataset.py
+++ dataset.py
@@ -3,7 +3,7 @@ from ucimlrepo import fetch_ucirepo
 HEART_DISEASE_ID = 45
 
 def pull_heart_disease_dataset():
-    data = fetch_ucirepo(HEART_DISEASE_ID)
+    data = fetch_ucirepo(id=HEART_DISEASE_ID)
 
     X = data.data.features
     y = data.data.targets
